89fbdd459f9774cbe5004ec7562c02cd77a03b5a
maadhudheeraj1999 <maadhudheeraj1999@gmail.com>
2026-04-29 00:23:28 +0530

    Update GitHub link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/raxx21"
+              href="https://github.com/maadhudheeraj1999"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
